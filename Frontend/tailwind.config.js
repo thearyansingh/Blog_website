a004cdd74b875a825fcc8c1f5eb1370a233244e5
thearyansingh <singharyan6999@gmail.com>
2024-12-26 23:22:56 +0530

    completed

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from 'daisyui'
+import daisyui from 'daisyui';
+
 export default {
   content: [
     "./index.html",
@@ -8,8 +9,7 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: {
-    daisyui
-},
-}
-
+  plugins: [
+    daisyui, // Correctly added as part of an array
+  ],
+};
