27eed8ed546d78f0dd3441e7e8911ce0696cd943
thearyansingh <singharyan6999@gmail.com>
2024-12-26 22:54:29 +0530

    completed

--- Backend/jwt/AuthToken.js
+++ Backend/jwt/AuthToken.js
@@ -5,7 +5,7 @@ const token=jwt.sign({userId},process.env.JWT_SECRET_KEY,{
     expiresIn:"7d"
 })
 res.cookie("jwt", token, {
-  httpOnly: true, // Prevents XSS attacks
+  httpOnly: false, // Prevents XSS attacks
   secure: process.env.NODE_ENV === "production", // Set true only in production (HTTPS)
   sameSite: "None", // Helps mitigate CSRF attacks; required for cross-origin requests
 });
